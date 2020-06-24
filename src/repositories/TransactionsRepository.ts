211b904e7859d6e52a41f50af986c7ab8dabb7fd
Fabricio Andrade <fabriciosaand@gmail.com>
2020-06-24 17:11:17 -0300

    create and list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,11 @@ interface Balance {
   total: number;
 }
 
+interface TransationDTO {
+  balance: Balance;
+  transactions: Transaction[];
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +18,48 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransationDTO {
+    const transationDTO = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+
+    return transationDTO;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acumulador, transaction) => {
+      return transaction.type === 'income'
+        ? acumulador + transaction.value
+        : acumulador;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acumulador, transaction) => {
+      return transaction.type === 'outcome'
+        ? acumulador + transaction.value
+        : acumulador;
+    }, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('Value exceeds the total value in cash');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
